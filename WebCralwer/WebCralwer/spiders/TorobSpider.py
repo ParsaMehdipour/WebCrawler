1a75d3e0ed716a4fd74d101c7a9b9193d725c842
a.zangane <a.zangane@TK-03>
2024-04-13 19:58:49 +0330

    * Name Edited (TorobSpider)

--- WebCralwer/WebCralwer/spiders/TorobSpider.py
+++ WebCralwer/WebCralwer/spiders/TorobSpider.py
@@ -5,7 +5,7 @@ from scrapy.loader import ItemLoader
 from WebCralwer.items import Product
 
 class TorobSpider(scrapy.Spider):
-    name = "torob"
+    name = "TorobSpider"
     allowed_domains = ["api.torob.com"]
     start_urls = ["https://api.torob.com/v4/base-product/search/?page=1&sort=popularity&size=100&category_name=%D9%85%D9%88%D8%A8%D8%A7%DB%8C%D9%84-%D9%88-%DA%A9%D8%A7%D9%84%D8%A7%DB%8C-%D8%AF%DB%8C%D8%AC%DB%8C%D8%AA%D8%A7%D9%84&category_id=175&category=175&source=next_desktop&suid=66191da888517ca4b24c6853&_bt__experiment=&_url_referrer="]
 
@@ -22,8 +22,8 @@ class TorobSpider(scrapy.Spider):
             loader.add_value('price', item['price'])
             loader.add_value('price_text', item['price_text'])
             loader.add_value('shop_text', item['shop_text'])
-            loader.add_value('delivery_city_name', item['delivery_city_name'])
-            loader.add_value('delivery_city_flag', item['delivery_city_flag'])
+            # loader.add_value('delivery_city_name', item['delivery_city_name'])
+            # loader.add_value('delivery_city_flag', item['delivery_city_flag'])
             yield loader.load_item()
 
         next_page = json_res['next']
