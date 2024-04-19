75bd683deecca5236efdc877528a28055d5d08f0
p.mehdipour <mehdipourparsa@gmail.com>
2024-04-19 20:24:25 +0330

    refactor : change minimum_threshold to 1 in monitors.py

--- WebCralwer/WebCralwer/monitors.py
+++ WebCralwer/WebCralwer/monitors.py
@@ -8,7 +8,7 @@ class ItemCountMonitor(Monitor):
     def test_minimum_number_of_items(self):
         item_extracted = getattr(
             self.data.stats, 'item_scraped_count', 0)
-        minimum_threshold = 10
+        minimum_threshold = 1
 
         msg = 'Extracted less than {} items'.format(
             minimum_threshold)
